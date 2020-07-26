065ed92c9d5af37e89e0eedb60858cdec4822717
muzstudy <muzstudy@192.168.0.12>
2020-07-26 10:12:32 -0400

    First Commit

---
+++ PearlWebAutomation/src/test/java/com/selenium/testcases/FirstTest.java
@@ -0,0 +1,11 @@
+package com.selenium.testcases;
+import org.testng.annotations.Test;
+
+
+public class FirstTest {
+	
+	@Test
+	public void myfirsttest() {
+		System.out.println("Run this test 5 times");
+	}
+}
